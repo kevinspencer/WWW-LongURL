f2f9890d3bf40ac1d40f07589ec3dd19abe418d4
Kevin Spencer <kevin@kevinspencer.org>
2011-08-22 01:16:04 -0700

    initial commit

---
+++ lib/WWW/LongURL.pm
@@ -0,0 +1,108 @@
+package WWW::LongURL;
+
+use JSON::Any;
+use LWP::UserAgent;
+use strict;
+use warnings;
+use base qw(Class::Accessor::Fast);
+
+__PACKAGE__->mk_accessors(qw(apibase useragent format shorturl longurl));
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+
+    my $self = {};
+    bless $self, $class;
+    $self->apibase('http://api.longurl.org/v2/');
+    $self->format('json');
+    $self->useragent('WWW-LongURL/0.01');
+    return $self;
+}
+
+sub expand {
+    my ($self, $url) = @_;
+
+    my $ua = LWP::UserAgent->new();
+    $ua->agent($self->useragent());
+
+    my $api = $self->apibase() . 'expand?url=' . $url . '&format=' . $self->format();
+    
+    my $response = $ua->get($api);
+    if ($response->is_success()) {
+        my $obj = JSON::Any->jsonToObj($response->decoded_content());
+        return $obj->{'long-url'};
+    } else {
+        print $response->status_line(), "\n";
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+WWW::LongURL - Perl interface to the LongURL API.
+
+=head1 SYNOPSIS
+
+  use WWW::LongURL;
+
+  my $longurl = WWW::LongURL->new();
+
+  my $expanded_url = $longurl->expand('http://bit.ly/cZcYFn');
+
+=head1 DESCRIPTION
+
+A simple interface for using the LongURL API to expand shortened URLs.
+
+You can expand a bit.ly URL like so:
+
+  my $url = $longurl->expand($some_bitly_url);
+
+=head2 METHODS
+
+=over 4
+
+=item C<new>
+
+Constructor
+
+=item C<expand($url)>
+
+On success, will return the expanded URL from LongURL.  On failure, returns undef.
+
+=back
+
+=head1 SUPPORTED SHORTENING SERVICES
+
+LongURL will expand the URL from over 200 supported shortening services.
+
+See L<http://longurl.org/services>
+
+=head1 REPOSITORY
+
+Development is on-going at:
+https://github.com/kevinspencer/WWW-LongURL
+
+=head1 AUTHOR
+
+Kevin Spencer, kspencer@cpan.org
+
+=head1 COPYRIGHT
+
+Copyright (c) 2011.  Kevin Spencer
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=head1 SEE ALSO
+
+L<http://longurl.org/>,
+L<http://longurl.org/api/>
+
+=cut
---
+++ t/www-longurl.t
@@ -0,0 +1,20 @@
+#!perl
+use strict;
+use warnings;
+use LWP::Online ':skip_all';
+
+# Skip online tests if we can't contact the LongURL API..
+BEGIN {
+    require Test::More;
+    unless ( LWP::Online::online() ) {
+        Test::More->import(
+            skip_all => 'Test requires a working internet connection'
+        );
+    }
+}
+
+use Test::More;
+use Test::Exception;
+
+use_ok('WWW::LongURL') or die;
+done_testing();
