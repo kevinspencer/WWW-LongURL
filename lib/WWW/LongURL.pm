cf6f569be0c3647b7f5596ac0042899505f79b2e
Kevin Spencer <kevin@kevinspencer.org>
2011-08-28 23:05:05 -0700

    added get_services()

--- lib/WWW/LongURL.pm
+++ lib/WWW/LongURL.pm
@@ -9,7 +9,7 @@ use base qw(Class::Accessor::Fast);
 
 __PACKAGE__->mk_accessors(qw(apibase useragent format error));
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub new {
     my $class = shift;
@@ -18,7 +18,7 @@ sub new {
     bless $self, $class;
     $self->apibase('http://api.longurl.org/v2/');
     $self->format('json');
-    $self->useragent('WWW-LongURL/0.04');
+    $self->useragent('WWW-LongURL/0.05');
     return $self;
 }
 
@@ -30,20 +30,37 @@ sub expand {
         return;
     }
 
+    my $request  = $self->apibase() . 'expand?url=' . uri_escape($url) . '&format=' . $self->format();
+    my $response = $self->_call_api($request);
+    return if (! $response);
+    if ($response->{'long-url'}) {
+        return $response->{'long-url'};
+    } else {
+        $self->error("Unrecognized response from " . $self->apibase());
+        return;
+    }
+}
+
+sub get_services {
+    my $self = shift;
+
+    my $request = $self->apibase() . 'services?&format=' . $self->format();
+    my $response = $self->_call_api($request);
+    return if (! $response);
+
+    # TODO: handle :utf8 by default
+    return keys (%$response);
+}
+
+sub _call_api {
+    my ($self, $what) = @_;
+
     my $ua = LWP::UserAgent->new();
     $ua->agent($self->useragent());
 
-    my $api = $self->apibase() . 'expand?url=' . uri_escape($url) . '&format=' . $self->format();
-    
-    my $response = $ua->get($api);
+    my $response = $ua->get($what);
     if ($response->is_success()) {
-        my $obj = JSON::Any->jsonToObj($response->decoded_content());
-        if ($obj->{'long-url'}) {
-            return $obj->{'long-url'};
-        } else {
-            $self->error("Unrecognized response from " . $self->apibase());
-            return;
-        }
+        return JSON::Any->jsonToObj($response->decoded_content());
     } else {
         $self->error($response->status_line());
         return;
@@ -66,7 +83,16 @@ WWW::LongURL - Perl interface to the LongURL API.
 
   my $expanded_url = $longurl->expand('http://bit.ly/cZcYFn');
   if (! $expanded_url) {
-     die $longurl->error(), "\n";
+      die $longurl->error(), "\n";
+  }
+
+  my @shortening_services = $longurl->get_services();
+  if (! @shortening_services) {
+      die $longurl->error(), "\n";
+  }
+  for my $service (@services) {
+      binmode(STDOUT, ':utf8');
+      print $service, "\n";
   }
 
 =head1 DESCRIPTION
@@ -76,6 +102,10 @@ A simple interface for using the LongURL API to expand shortened URLs.
 You can expand a bit.ly URL like so:
 
   my $url = $longurl->expand($some_bitly_url);
+
+You can retrieve a list of shortening services supported by longurl like so:
+
+  my @services = $longurl->get_services();
 
 =head2 METHODS
 
@@ -88,6 +118,10 @@ Constructor
 =item C<expand($url)>
 
 On success, will return the expanded URL from LongURL.  On failure, returns undef.
+
+=item C<get_services>
+
+On success, returns an array of shortening services that longurl.org supports.  On failure, returns undef.
 
 =item C<error>
 
